9edfbc78c7a788b54c3a9463fc87326331a17889
Mher-777 <mherjan2003@mail.ru>
2020-04-20 22:12:36 +0400

    added featured-product section

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -14,6 +14,27 @@ $(function () {
 			selectVisibleOptions: 5,
 		});
 	}
+	let mainRated = () => {
+		$('.rated.rated--default').rateYo({
+			rating: 4.5,
+			starWidth: "15px",
+			normalFill: "#ffc000",
+			readOnly: true,
+		})
+	}
+	let featuredSlider = () => {
+		$('.js-featured-product__slider').slick({
+			appendArrows: '.featured-product__control',
+			autoplay: true,
+			autoplaySpeed: 3000,
+			speed: 400,
+			cssEase: 'linear',
+			nextArrow: '<button class="slider-arrow featured-product__arrow featured-product__arrow--right"><svg class="svg-sprite-icon icon-chevron-right"><use xlink:href="static/images/sprite/symbol/sprite.svg#chevron-right"></use></svg></button>',
+			prevArrow: '<button class="slider-arrow featured-product__arrow featured-product__arrow--left"><svg class="svg-sprite-icon icon-chevron-left"><use xlink:href="static/images/sprite/symbol/sprite.svg#chevron-left"></use></svg></button>',
+		})
+	}
 	formStyler()
 	basketTutorial()
+	mainRated()
+	featuredSlider()
 })
--- gulp/tasks/vendorsJS.js
+++ gulp/tasks/vendorsJS.js
@@ -4,7 +4,9 @@ const concat = require('gulp-concat');
 const vendorsScripts = [
   'node_modules/svg4everybody/dist/svg4everybody.min.js',
   'node_modules/jquery/dist/jquery.min.js',
-  'node_modules/jquery-form-styler/dist/jquery.formstyler.min.js'
+  'node_modules/jquery-form-styler/dist/jquery.formstyler.min.js',
+  'node_modules/rateyo/min/jquery.rateyo.min.js',
+  'node_modules/slick-carousel/slick/slick.js'
 ];
 
 module.exports = function vendors(cb) {
