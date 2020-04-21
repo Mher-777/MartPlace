2eda453e7c4732f9550b87d13d27a430f8dc4cc5
Mher-777 <mherjan2003@mail.ru>
2020-04-22 00:33:50 +0400

    added followers-feed section

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -52,18 +52,42 @@ $(function () {
 			"effects": "fade rotateX(70deg)"
 		}
 	});
-
-
-
-
 	}
+	let followersFeedSlider = () => {
+		$('.js-followers-feed__slider').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			appendArrows: '.followers-feed__control',
+			// autoplay: true,
+			autoplaySpeed: 3000,
+			speed: 400,
+			cssEase: 'linear',
+			nextArrow: '<button class="slider-arrow followers-feed__arrow followers-feed__arrow--right"><svg class="svg-sprite-icon icon-chevron-right"><use xlink:href="static/images/sprite/symbol/sprite.svg#chevron-right"></use></svg></button>',
+			prevArrow: '<button class="slider-arrow followers-feed__arrow followers-feed__arrow--left"><svg class="svg-sprite-icon icon-chevron-left"><use xlink:href="static/images/sprite/symbol/sprite.svg#chevron-left"></use></svg></button>',
+		})
+	}
+	let sliderSpace = () => {
+		$('[data-space]').each(function () {
+			let $this = $(this),
+				$space = $this.attr('data-space');
+			$('.slick-slide').css({
+				marginLeft: $space + 'px',
+				marginRight: $space + 'px'
+			});
+			$('.slick-list').css({
+				marginLeft: -$space + 'px',
+				marginRight: -$space/2 + 'px'
+			})
+		});
+	}
+
 
 	formStyler()
 	basketTutorial()
 	mainRated()
 	featuredSlider()
 	filterDropDown()
+	followersFeedSlider()
 	newestProductFilter()
-
-
+	sliderSpace()
 })
